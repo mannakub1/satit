11c57b766f024754280f37fa7d547e25427a04ed
Nanthipath <omliler_man@hotmail.com>
2017-09-25 15:42:32 +0700

    manny last

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,9 @@ var token = localStorage.getItem('token');
 //var address = "http://192.168.217.102:3000/";
 // var address = 'http://localhost:3000/'
 //var address = 'http://202.28.73.138:3000/'
-var address = 'http://172.27.169.121:3000/'
+// var address = 'http://172.27.169.121:3000/'
+// var address = 'http://172.27.225.177:4000/'
+var address = 'http://localhost:3000/'
 // var address = 'http://172.27.225.177:3000/'
 //var address = 'http://172.27.160.80:3000/'
 // var address = "http://172.27.160.166:3000/";
@@ -716,9 +718,46 @@ myApp.controller('mainCtrl',  function($scope, $http, fileUpload) {
 
 });
 
+myApp.controller('stdGrade', function($scope, $http, fileUpload) {
+	$scope.token = localStorage.getItem('token');
+	$scope.student = JSON.parse(sessionStorage.getItem('stdData'));
+	$scope.Grade =  [];
+	
+
+	$scope.getGrade = function () {
+		path = address + "api/student/courses?student_id=" + $scope.student.id;
+		$http.get(path , {headers: {'token': $scope.token} })
+			.success(function(data){
+				console.log(data);
+				console.log("5555");
+				$scope.stdRoom = data.student_room;
+				console.log($scope.stdRoom);
+			})
+			.error(function(data, status, headers, config) {
+				if(data.error === 'token expired'){
+					window.location.href = 'login.html';;
+				}
+			});
+	}
+
+	$scope.collapseGrade = function () {
+		var e = document.getElementById("selectGradeLevel").value;
+		for(var i = 0; i < $scope.Grade.length; ++i){
+			$scope.Grade[i] = false;
+		}
+		$scope.Grade[e] = true;
+
+	}
+
+	$scope.getGrade();
+
+	
+
+});
+
 myApp.controller('stdCtrl',  function($scope, $http, fileUpload) {
 	$scope.showHome = true;
-		$scope.student = JSON.parse(sessionStorage.getItem('stdData'));
+	$scope.student = JSON.parse(sessionStorage.getItem('stdData'));
 
 	console.log($scope.student);
 	$scope.token = localStorage.getItem('token');
@@ -741,7 +780,7 @@ myApp.controller('stdCtrl',  function($scope, $http, fileUpload) {
 	$scope.showEditGuardian = false;
 	$scope.showStudentData = false;
 	$scope.showAddStudent = false;
-	//console.log($scope.userType);
+	
 
 	$scope.showStudentDataBtn = function() {
 		$scope.student = JSON.parse(sessionStorage.getItem('stdData'));
